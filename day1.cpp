a38096597e3f3f2ff2b867ee60f16c08f3229863
AvishiShrivastava <116447177+AvishiShrivastava@users.noreply.github.com>
2024-10-01 23:50:31 +0530

    Added ACC45DAYSOFCODE Day 1 Code

---
+++ day1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+    int A, B, C, n;
+    
+    
+    cin >> A >> B >> C >> n;
+    
+    
+    if (n == A || n == B || n == C) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    
+    return 0;
+}
+
+
